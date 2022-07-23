868b6d20d387dac6b5493f74e73e48a0f6120930
AlekseyG1995 <alekseygudkov1995@gmail.com>
2022-07-24 02:51:40 +0300

    browserRouter, empty pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-function App() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
-}
+import { Routes, Route } from "react-router-dom"
+import { AddEmployeePage } from "./pages/AddEmployeePage"
+import { EditorEmployeePage } from "./pages/EditorEmployeePage"
+import { ErrorPage } from "./pages/ErrorPage"
+import { ViewEmployeePage } from "./pages/ViewEmployeePage"
 
-export default App;
+export const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<ViewEmployeePage />}></Route>
+        <Route path="/edit" element={<EditorEmployeePage />}></Route>
+        <Route path="/add" element={<AddEmployeePage />}></Route>
+        <Route path="*" element={<ErrorPage />}></Route>
+      </Routes>
+    </>
+  )
+}
