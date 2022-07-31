e317a2ee61ca86224551b3bc51c448f9c7da3ad4
AlekseyG1995 <alekseygudkov1995@gmail.com>
2022-07-31 19:14:25 +0300

    UI: Loader fix

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,7 +2,7 @@ import { FC } from "react"
 
 export const Loader:FC = () => (
   <>
-    <div className="flex items-center justify-center space-x-5 animate-bounce">
+    <div className="flex items-center justify-center h-screen space-x-5 animate-bounce">
       <div className="w-6 h-6 bg-blue-500 rounded-full"></div>
       <div className="w-6 h-6 bg-red-600 rounded-full"></div>
       <div className="w-6 h-6 bg-blue-500 rounded-full"></div>
